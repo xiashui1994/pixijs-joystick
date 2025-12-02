a9da2a2cb6472642bdf3982d474e4f3818623267
lijunchang <lijunchang@itixiao.com>
2025-12-02 22:18:13 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    target: 'es2015',
+    lib: {
+      entry: './lib/main.ts',
+      name: 'Counter',
+      fileName: 'counter',
+    },
+  },
+  plugins: [dts({
+    staticImport: true,
+    insertTypesEntry: true,
+  })],
+})
